968487a23bc1885feff0eef815217ba180ae873a
Priti Desai <pdesai@us.ibm.com>
2017-04-04 14:28:12 -0700

    Update trigger.go

--- commands/trigger.go
+++ commands/trigger.go
@@ -21,7 +21,7 @@ import (
     "fmt"
 
     "github.com/openwhisk/openwhisk-client-go/whisk"
-    "../wski18n"
+    "github.com/openwhisk/openwhisk-cli/wski18n"
 
     "github.com/spf13/cobra"
     "github.com/fatih/color"
