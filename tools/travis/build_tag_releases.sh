f68e343150fc112ab2f6f9b90521741131f7d2a8
Vincent Hou <shou@us.ibm.com>
2017-04-17 11:59:38 -0400

    Add the tag "latest" once when a commit is merged
    Since we add osx as a second build environment, the latest tag
    only needs to be added once. The linux build releases the linux binary
    and the osx build releases the mac binary.

--- tools/travis/build_tag_releases.sh
+++ tools/travis/build_tag_releases.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-declare -a os_list=("linux" "darwin" "windows")
 arc=amd64
 build_file_name=${1:-"wsk"}
 zip_file_name=${2:-"OpenWhisk_CLI"}
+os=$TRAVIS_OS_NAME
 
-for os in "${os_list[@]}"
-do
-    wsk=$build_file_name
-    if [ "$os" == "windows" ]; then
-        wsk="$wsk.exe"
-    fi
-    cd $TRAVIS_BUILD_DIR
-    GOOS=$os GOARCH=$arc go build -o build/$os/$wsk
-    cd build/$os
-    zip -r "$TRAVIS_BUILD_DIR/$zip_file_name-$TRAVIS_TAG-$os-$arc.zip" $wsk
-done
+if [ "$os" == "osx" ]; then
+    os="darwin"
+fi
+wsk=$build_file_name
+if [ "$os" == "windows" ]; then
+    wsk="$wsk.exe"
+fi
+cd $TRAVIS_BUILD_DIR
+GOOS=$os GOARCH=$arc go build -o build/$os/$wsk
+cd build/$os
+zip -r "$TRAVIS_BUILD_DIR/$zip_file_name-$TRAVIS_TAG-$os-$arc.zip" $wsk
