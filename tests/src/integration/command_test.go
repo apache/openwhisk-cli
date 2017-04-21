460079fe326cbd0daea200408a170068f3893887
Vincent Hou <shou@us.ibm.com>
2017-04-21 12:15:43 -0400

    Add the integration test to set APIHost, Auth and Namespace
    As the Travis CI has set up for Mac and Ubuntu, the openwhisk integration
    tests and the native command tests can be continously added. This PR
    adds the test to set APIHost, Auth and Namespace.

--- tests/src/integration/command_test.go
+++ tests/src/integration/command_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 var wsk *common.Wsk = common.NewWsk()
+var tmpProp = os.Getenv("GOPATH") + "/src/github.com/openwhisk/openwhisk-cli/wskprops.tmp"
 
 func TestHelpUsageInfoCommand(t *testing.T) {
 	stdout, err := wsk.RunCommand("-h")
@@ -39,6 +40,21 @@ func TestShowAPIVersion(t *testing.T) {
 	assert.Equal(t, nil, err, "The command property get --apiversion failed to run.")
 	assert.Contains(t, string(stdout), "whisk API version",
 		"The output of the command property get --apiversion does not contain \"whisk API version\".")
+}
+
+// Test case to verify the default namespace _.
+func TestDefaultNamespace(t *testing.T) {
+	common.CreateFile(tmpProp)
+	common.WriteFile(tmpProp, []string{"NAMESPACE="})
+
+	os.Setenv("WSK_CONFIG_FILE", tmpProp)
+	assert.Equal(t, os.Getenv("WSK_CONFIG_FILE"), tmpProp, "The environment variable WSK_CONFIG_FILE has not been set.")
+
+	stdout, err := wsk.RunCommand("property", "get", "-i", "--namespace")
+	assert.Equal(t, nil, err, "The command property get -i --namespace failed to run.")
+	assert.Contains(t, common.RemoveRedundentSpaces(string(stdout)), "whisk namespace _",
+		"The output of the command does not contain \"whisk namespace _\".")
+	common.DeleteFile(tmpProp)
 }
 
 
