835dff62a7d965bd1cb9b2cfb7feb7129f83665f
Vincent Hou <shou@us.ibm.com>
2017-05-02 16:54:31 -0400

    Add test cases regarding the action

--- tests/src/integration/common/wsk.go
+++ tests/src/integration/common/wsk.go
@@ -1,51 +1,86 @@
 package common
 
 import (
-	"os"
-	"os/exec"
+    "os"
+    "os/exec"
+    "strings"
 )
 
 const cmd = "./wsk"
 const arg = "-i"
 
 type Wsk struct {
-	Path string
-	Arg []string
-	Dir string
-	Wskprops *Wskprops
+    Path string
+    Arg []string
+    Dir string
+    Wskprops *Wskprops
 }
 
 func NewWsk() *Wsk {
-	return NewWskWithPath(os.Getenv("GOPATH") + "/src/github.com/openwhisk/openwhisk-cli/")
+    return NewWskWithPath(os.Getenv("GOPATH") + "/src/github.com/openwhisk/openwhisk-cli/")
 }
 
 func NewWskWithPath(path string) *Wsk {
-	var dep Wsk
-	dep.Path = cmd
-	dep.Arg = []string{arg}
-	dep.Dir = path
-	dep.Wskprops = GetWskprops()
-	return &dep
+    var dep Wsk
+    dep.Path = cmd
+    dep.Arg = []string{arg}
+    dep.Dir = path
+    dep.Wskprops = GetWskprops()
+    return &dep
 }
 
 func (wsk *Wsk)Exists() bool {
-	_, err := os.Stat(wsk.Dir + wsk.Path);
-	if err == nil {
-		return true
-	} else {
-		return false
-	}
+    _, err := os.Stat(wsk.Dir + wsk.Path);
+    if err == nil {
+        return true
+    } else {
+        return false
+    }
 }
 
 func (wsk *Wsk)RunCommand(s ...string) ([]byte, error) {
-	cs := wsk.Arg
-	cs = append(cs, s...)
-	command := exec.Command(wsk.Path, cs...)
-	command.Dir = wsk.Dir
-	return command.CombinedOutput()
+    cs := wsk.Arg
+    cs = append(cs, s...)
+    command := exec.Command(wsk.Path, cs...)
+    command.Dir = wsk.Dir
+    return command.CombinedOutput()
 }
 
 func (wsk *Wsk)ListNamespaces() ([]byte, error) {
-	return wsk.RunCommand("namespace", "list", "--apihost", wsk.Wskprops.APIHost,
-		"--auth", wsk.Wskprops.AuthKey)
+    return wsk.RunCommand("namespace", "list", "--apihost", wsk.Wskprops.APIHost,
+        "--auth", wsk.Wskprops.AuthKey)
+}
+
+func (wsk *Wsk)Getfqn(name string) string {
+    sep := "/"
+    if (strings.HasPrefix(name, sep)) {
+        return name
+    } else {
+        return sep + wsk.Wskprops.Namespace + sep + name
+    }
+}
+
+func (wsk *Wsk)CreateAction(name string, artifact string) ([]byte, error) {
+    return wsk.CreateUpdateAction(name, artifact, false)
+}
+
+func (wsk *Wsk)UpdateAction(name string, artifact string) ([]byte, error) {
+    return wsk.CreateUpdateAction(name, artifact, true)
+}
+
+func (wsk *Wsk)CreateUpdateAction(name string, artifact string, update bool) ([]byte, error) {
+    arg := []string{"action"}
+    if update {
+        arg = append(arg, "update")
+    } else {
+        arg = append(arg, "create")
+    }
+    arg = append(arg, wsk.Getfqn(name), artifact, "--apihost", wsk.Wskprops.APIHost,
+        "--auth", wsk.Wskprops.AuthKey, "--apiversion", wsk.Wskprops.APIVersion)
+    return wsk.RunCommand(arg...)
+}
+
+func (wsk *Wsk)DeleteAction(name string) ([]byte, error) {
+    return wsk.RunCommand("action", "delete", wsk.Getfqn(name), "--apihost", wsk.Wskprops.APIHost,
+        "--auth", wsk.Wskprops.AuthKey, "--apiversion", wsk.Wskprops.APIVersion)
 }
--- tests/src/integration/common/wskprops.go
+++ tests/src/integration/common/wskprops.go
@@ -1,39 +1,41 @@
 package common
 
 import (
-	"github.com/spf13/viper"
-	"io/ioutil"
-	"os"
+    "github.com/spf13/viper"
+    "io/ioutil"
+    "os"
 )
 
 type Wskprops struct {
-	APIHost string
-	APIVersion string
-	AuthKey string
-	ControllerHost string
-	ControllerPort string
+    APIHost string
+    APIVersion string
+    AuthKey string
+    Namespace string
+    ControllerHost string
+    ControllerPort string
 }
 
 func GetWskprops() *Wskprops {
-	var dep Wskprops
-	dep.APIHost = ""
-	dep.AuthKey = ""
-	dep.APIVersion = "v1"
+    var dep Wskprops
+    dep.APIHost = ""
+    dep.AuthKey = ""
+    dep.APIVersion = "v1"
+    dep.Namespace = "_"
 
-	viper.SetConfigName("whisk")
-	viper.AddConfigPath(os.Getenv("OPENWHISK_HOME"))
+    viper.SetConfigName("whisk")
+    viper.AddConfigPath(os.Getenv("OPENWHISK_HOME"))
 
-	err := viper.ReadInConfig()
-	if err == nil {
-		authPath := viper.GetString("testing.auth")
+    err := viper.ReadInConfig()
+    if err == nil {
+        authPath := viper.GetString("testing.auth")
 
-		b, err := ioutil.ReadFile(authPath)
-		if err == nil {
-			dep.AuthKey = string(b)
-		}
-		dep.APIHost = viper.GetString("router.host")
-		dep.ControllerHost = viper.GetString("router.host")
-		dep.ControllerPort = viper.GetString("controller.host.port")
-	}
-	return &dep
+        b, err := ioutil.ReadFile(authPath)
+        if err == nil {
+            dep.AuthKey = string(b)
+        }
+        dep.APIHost = viper.GetString("router.host")
+        dep.ControllerHost = viper.GetString("router.host")
+        dep.ControllerPort = viper.GetString("controller.host.port")
+    }
+    return &dep
 }
