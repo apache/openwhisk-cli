bb4becc794720881cacc41afacdd14663d7da9bc
Vincent Hou <shou@us.ibm.com>
2017-05-11 11:41:13 -0400

    Run the openwhisk test cases by using the binary generated by the cli repo

--- tools/travis/install_openwhisk.sh
+++ tools/travis/install_openwhisk.sh
@@ -12,21 +12,25 @@ WHISKDIR="$HOMEDIR/openwhisk"
 cd $WHISKDIR
 ./tools/travis/setup.sh
 
-ANSIBLE_CMD="ansible-playbook -i environments/local"
+ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=testing"
 
 cd $WHISKDIR/ansible
 $ANSIBLE_CMD setup.yml
 $ANSIBLE_CMD prereq.yml
 $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
+$ANSIBLE_CMD apigateway.yml
 
 cd $WHISKDIR
-./gradlew distDocker
+./gradlew distDocker -PdockerImagePrefix=testing
 
 cd $WHISKDIR/ansible
 $ANSIBLE_CMD wipe.yml
 $ANSIBLE_CMD openwhisk.yml
 
+# Copy the binary generated into the OPENWHISK_HOME/bin, so that the test cases will run based on it.
+cp $TRAVIS_BUILD_DIR/wsk $WHISKDIR/bin
+
 # Run the test cases under openwhisk to ensure the quality of the binary.
 cd $WHISKDIR
-./gradlew tests:test
+./gradlew :tests:testLean
