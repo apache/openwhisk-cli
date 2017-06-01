e06380de5817b36f183e2fa55b6ccff45bf4eda7
Matt Rutkowski <mrutkows@us.ibm.com>
2017-06-01 14:50:45 -0500

    Add scancode to travis and fix all scancode errors (#46)
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Add scanCode to Travis and fix all scanCode errors.
    
    * Update i18n_resource.go with most current timestamps.

--- tests/src/integration/common/utils.go
+++ tests/src/integration/common/utils.go
@@ -1,87 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package common
 
 import (
-	"fmt"
-	"os"
-	"unicode"
-	"io"
-	"strings"
+    "fmt"
+    "os"
+    "unicode"
+    "io"
+    "strings"
 )
 
 func checkError(err error) {
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(0)
-	}
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(0)
+    }
 }
 
 func CreateFile(filePath string) {
-	var _, err = os.Stat(filePath)
+    var _, err = os.Stat(filePath)
 
-	if os.IsNotExist(err) {
-		var file, err = os.Create(filePath)
-		checkError(err)
-		defer file.Close()
-	}
-	return
+    if os.IsNotExist(err) {
+        var file, err = os.Create(filePath)
+        checkError(err)
+        defer file.Close()
+    }
+    return
 }
 
 func ReadFile(filePath string) string {
-	var file, err = os.OpenFile(filePath, os.O_RDWR, 0644)
-	checkError(err)
-	defer file.Close()
+    var file, err = os.OpenFile(filePath, os.O_RDWR, 0644)
+    checkError(err)
+    defer file.Close()
 
-	var text = make([]byte, 1024)
-	for {
-		n, err := file.Read(text)
-		if err != io.EOF {
-			checkError(err)
-		}
-		if n == 0 {
-			break
-		}
-	}
-	return string(text)
+    var text = make([]byte, 1024)
+    for {
+        n, err := file.Read(text)
+        if err != io.EOF {
+            checkError(err)
+        }
+        if n == 0 {
+            break
+        }
+    }
+    return string(text)
 }
 
 func WriteFile(filePath string, lines []string) {
-	var file, err = os.OpenFile(filePath, os.O_RDWR, 0644)
-	checkError(err)
-	defer file.Close()
+    var file, err = os.OpenFile(filePath, os.O_RDWR, 0644)
+    checkError(err)
+    defer file.Close()
 
-	for _, each := range lines {
-		_, err = file.WriteString(each + "\n")
-		checkError(err)
-	}
+    for _, each := range lines {
+        _, err = file.WriteString(each + "\n")
+        checkError(err)
+    }
 
-	err = file.Sync()
-	checkError(err)
+    err = file.Sync()
+    checkError(err)
 }
 
 func DeleteFile(filePath string) {
-	var err = os.Remove(filePath)
-	checkError(err)
+    var err = os.Remove(filePath)
+    checkError(err)
 }
 
 func RemoveRedundentSpaces(in string) (out string) {
-	white := false
-	for _, c := range in {
-		if unicode.IsSpace(c) {
-			if !white {
-				out = out + " "
-			}
-			white = true
-		} else {
-			out = out + string(c)
-			white = false
-		}
-	}
-	out = strings.TrimSpace(out)
-	return
+    white := false
+    for _, c := range in {
+        if unicode.IsSpace(c) {
+            if !white {
+                out = out + " "
+            }
+            white = true
+        } else {
+            out = out + string(c)
+            white = false
+        }
+    }
+    out = strings.TrimSpace(out)
+    return
 }
 
 func GetTestActionFilename(fileName string) string {
-	return GetRepoPath() + "/tests/src/dat/" + fileName
+    return GetRepoPath() + "/tests/src/dat/" + fileName
 }
 
 func GetRepoPath() string {
@@ -89,6 +105,6 @@ func GetRepoPath() string {
 }
 
 type InvalidArg struct {
-	Cmd []string
-	Err string
+    Cmd []string
+    Err string
 }
--- tests/src/integration/common/wskprops.go
+++ tests/src/integration/common/wskprops.go
@@ -1,39 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package common
 
 import (
-	"github.com/spf13/viper"
-	"io/ioutil"
-	"os"
+    "github.com/spf13/viper"
+    "io/ioutil"
+    "os"
 )
 
 type Wskprops struct {
-	APIHost string
-	APIVersion string
-	AuthKey string
-	ControllerHost string
-	ControllerPort string
+    APIHost string
+    APIVersion string
+    AuthKey string
+    ControllerHost string
+    ControllerPort string
 }
 
 func GetWskprops() *Wskprops {
-	var dep Wskprops
-	dep.APIHost = ""
-	dep.AuthKey = ""
-	dep.APIVersion = "v1"
+    var dep Wskprops
+    dep.APIHost = ""
+    dep.AuthKey = ""
+    dep.APIVersion = "v1"
 
-	viper.SetConfigName("whisk")
-	viper.AddConfigPath(os.Getenv("OPENWHISK_HOME"))
+    viper.SetConfigName("whisk")
+    viper.AddConfigPath(os.Getenv("OPENWHISK_HOME"))
 
-	err := viper.ReadInConfig()
-	if err == nil {
-		authPath := viper.GetString("testing.auth")
+    err := viper.ReadInConfig()
+    if err == nil {
+        authPath := viper.GetString("testing.auth")
 
-		b, err := ioutil.ReadFile(authPath)
-		if err == nil {
-			dep.AuthKey = string(b)
-		}
-		dep.APIHost = viper.GetString("router.host")
-		dep.ControllerHost = viper.GetString("router.host")
-		dep.ControllerPort = viper.GetString("controller.host.port")
-	}
-	return &dep
+        b, err := ioutil.ReadFile(authPath)
+        if err == nil {
+            dep.AuthKey = string(b)
+        }
+        dep.APIHost = viper.GetString("router.host")
+        dep.ControllerHost = viper.GetString("router.host")
+        dep.ControllerPort = viper.GetString("controller.host.port")
+    }
+    return &dep
 }
