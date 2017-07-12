9c617109ac30eca10b3efa1814317a56a8a8b263
Vincent Hou <shou@us.ibm.com>
2017-07-11 22:22:29 -0400

    Revert "Test cases sync (#81)"
    This reverts commit 193c3c5731439eb0f1f03d56ee79f5b3a1e3734b.

--- tests/dat/actions/corsHeaderMod.js
+++ tests/dat/actions/corsHeaderMod.js
@@ -2,10 +2,7 @@ function main() {
     return {
         headers: {
             "Access-Control-Allow-Origin": "Origin set from Web Action",
-            "Access-Control-Allow-Headers": "Headers set from Web Action",
-            "Access-Control-Allow-Methods": "Methods set from Web Action",
-            "Location": "openwhisk.org",
-            "Set-Cookie": "cookie-cookie-cookie"
+            "Access-Control-Allow-Headers": "Headers set from Web Action"
         },
         code: 200
     }
