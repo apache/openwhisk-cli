083cbe1ad2e72d01da973e1b9d335f2a98ab50c0
Vincent Hou <shou@us.ibm.com>
2017-08-17 12:17:54 -0400

    Pull the openwhisk image instead of building it

--- tools/travis/test_openwhisk.sh
+++ tools/travis/test_openwhisk.sh
@@ -15,7 +15,7 @@ WHISKDIR="$HOMEDIR/incubator-openwhisk"
 cd $WHISKDIR
 ./tools/travis/setup.sh
 
-ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=testing"
+ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=openwhisk"
 
 cd $WHISKDIR/ansible
 $ANSIBLE_CMD setup.yml
@@ -24,17 +24,16 @@ $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
 $ANSIBLE_CMD apigateway.yml
 
-cd $WHISKDIR
-GRADLE_PROJS_SKIP="-x :core:pythonAction:distDocker -x :core:python2Action:distDocker -x :core:swift3Action:distDocker -x :core:javaAction:distDocker"
-TERM=dumb ./gradlew distDocker -PdockerImagePrefix=testing $GRADLE_PROJS_SKIP
+cd $TRAVIS_BUILD_DIR
+TERM=dumb ./gradlew buildBinaries
 
 cd $WHISKDIR/ansible
 $ANSIBLE_CMD wipe.yml
-$ANSIBLE_CMD openwhisk.yml
+$ANSIBLE_CMD openwhisk.yml -e openwhisk_cli_home=$TRAVIS_BUILD_DIR
 
 # Copy the binary generated into the OPENWHISK_HOME/bin, so that the test cases will run based on it.
 mkdir -p $WHISKDIR/bin
-cp $TRAVIS_BUILD_DIR/wsk $WHISKDIR/bin
+cp $TRAVIS_BUILD_DIR/bin/wsk $WHISKDIR/bin
 
 # Run the test cases under openwhisk to ensure the quality of the binary.
 cd $TRAVIS_BUILD_DIR
