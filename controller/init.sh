0aadcabc2fba4c1fdfa08d17960448c59e1d746e
Ubuntu <k.devara@samsung.com>
2018-01-02 08:58:02 +0000

    "remove controller and invoker from this repo"

--- controller/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export CONTROLLER_OPTS
-CONTROLLER_OPTS="$CONTROLLER_OPTS -Dakka.remote.netty.tcp.bind-hostname=$(hostname -I) $(./transformEnvironment.sh)"
-
-exec controller/bin/controller "$@"
--- invoker/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export INVOKER_OPTS
-INVOKER_OPTS="$INVOKER_OPTS $(./transformEnvironment.sh)"
-
-exec invoker/bin/invoker "$@"
