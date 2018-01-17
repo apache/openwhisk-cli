48e52d5ab141d1c3c075a24ad8c1d73cf4767dbf
Jonathan Springer <jonpspri@gmail.com>
2018-01-17 11:24:51 -0500

    Move git clones to .travis.yml

--- tools/travis/test_openwhisk.sh
+++ tools/travis/test_openwhisk.sh
@@ -9,6 +9,18 @@ set -e
 #
 #    ./gradlew buildBinaries release --PcrossCompile=true
 #
+#  Also at this point, you should already have incubator-openwhisk pulled down
+#  from gradle in the parent directory, using a command such as:
+#
+#    git clone --depth 3 https://github.com/apache/incubator-openwhisk.git
+#
+#  To be clear, your directory structure will look something like...
+#
+#      $HOMEDIR
+#       |- incubator-openwhisk
+#       |- incubator-openwhisk-cli (This project)
+#       |- incubator-openwhisk-utilities (For scancode)
+#
 #  The idea is to only build once and to be transparent about building in
 #  the Travis script.  To that end, some of the other builds that had been
 #  done in this script will be moved into Travis.yml.
@@ -19,32 +31,29 @@ set -e
 #  when running tests of the script.
 #
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-travis_build_dir="$( cd "${TRAVIS_BUILD_DIR:-$scriptdir/../..}" && pwd )"
+
+TRAVIS_BUILD_DIR="$( cd "${TRAVIS_BUILD_DIR:-$scriptdir/../..}" && pwd )"
+export TRAVIS_BUILD_DIR
+
+# For the gradle builds.
+HOMEDIR="$(dirname "$TRAVIS_BUILD_DIR")"
+OPENWHISK_HOME="$( cd "${OPENWHISK_HOME:-$HOMEDIR/incubator-openwhisk}" && pwd )"
+export OPENWHISK_HOME
 
 #
 #  These are the basic tests
 #
-cd $travis_build_dir
+cd $TRAVIS_BUILD_DIR
 ./tools/travis/scancode.sh
 
 #  Run separate test scopes as separate
 ./gradlew --console=plain goLint
 ./gradlew --console=plain goTest -PgoTags=unit
-export PATH=$PATH:$travis_build_dir
+export PATH=$PATH:$TRAVIS_BUILD_DIR
 ./gradlew --console=plain goTest -PgoTags=native
 
-HOMEDIR="$(dirname "$travis_build_dir")"
-OPENWHISK_HOME="$HOMEDIR/incubator-openwhisk"
-export OPENWHISK_HOME
-
-# Clone the OpenWhisk code
-# TODO: Probably not necessary because the build now requires the
-#       incubator-openwhisk directory as well.
-if [ ! -e "$OPENWHISK_HOME" ]; then
-    cd $HOMEDIR
-    git clone --depth 3 https://github.com/apache/incubator-openwhisk.git
-fi
-
+#
+#  Set up the OpenWhisk environment ( TODO: reusable script for incubtor-openwhisk? )
 cd $OPENWHISK_HOME
 ./tools/travis/setup.sh
 
@@ -57,21 +66,17 @@ $ANSIBLE_CMD prereq.yml
 $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
 $ANSIBLE_CMD apigateway.yml
-
-#  A quick nested build -- probably not needed.
-#( cd $travis_build_dir && ./gradlew --console=plain buildBinaries )
-
 $ANSIBLE_CMD wipe.yml
 # TODO -- Some flag might be needed to get CLI from local directory (?)
-$ANSIBLE_CMD openwhisk.yml -e openwhisk_cli_home=$travis_build_dir
+$ANSIBLE_CMD openwhisk.yml -e openwhisk_cli_home=$TRAVIS_BUILD_DIR
 
 # Copy the binary generated into the OPENWHISK_HOME/bin, so that the test cases will run based on it.
 # TODO - if the ansible above were correctly configured, this wouldn't be necessary
 mkdir -p $OPENWHISK_HOME/bin
-cp -f $travis_build_dir/bin/wsk $OPENWHISK_HOME/bin
+cp -f $TRAVIS_BUILD_DIR/bin/wsk $OPENWHISK_HOME/bin
 
 # Run the test cases under openwhisk to ensure the quality of the binary.
-cd $travis_build_dir
+cd $TRAVIS_BUILD_DIR
 
 ./gradlew --console=plain :tests:test -Dtest.single=*ApiGwCliTests*
 #sleep 30
