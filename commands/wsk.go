2a2b2af18f3a4fceea4ca68a01f9fa0e6b78710f
dubeejw <jwdubee@us.ibm.com>
2018-01-27 10:16:57 -0500

    Go format source code

--- commands/wsk.go
+++ commands/wsk.go
@@ -18,53 +18,53 @@
 package commands
 
 import (
-    "github.com/spf13/cobra"
-    "github.com/apache/incubator-openwhisk-cli/wski18n"
+	"github.com/apache/incubator-openwhisk-cli/wski18n"
+	"github.com/spf13/cobra"
 )
 
 // WskCmd defines the entry point for the cli.
 var WskCmd = &cobra.Command{
-    Use:              "wsk",
-    Short:            wski18n.T("OpenWhisk cloud computing command line interface."),
-    Long:             logoText(),
-    SilenceUsage:     true,
-    PersistentPreRunE:parseConfigFlags,
+	Use:               "wsk",
+	Short:             wski18n.T("OpenWhisk cloud computing command line interface."),
+	Long:              logoText(),
+	SilenceUsage:      true,
+	PersistentPreRunE: parseConfigFlags,
 }
 
 var listCmd = &cobra.Command{
-    Use:   "list",
-    Short: wski18n.T("list entities in the current namespace"),
-    SilenceUsage:   true,
-    SilenceErrors:  true,
-    PreRunE: SetupClientConfig,
-    RunE:   namespaceGetCmd.RunE,
+	Use:           "list",
+	Short:         wski18n.T("list entities in the current namespace"),
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	PreRunE:       SetupClientConfig,
+	RunE:          namespaceGetCmd.RunE,
 }
 
 func init() {
-    WskCmd.SetHelpTemplate(`{{with or .Long .Short }}{{.}}
+	WskCmd.SetHelpTemplate(`{{with or .Long .Short }}{{.}}
 {{end}}{{if or .Runnable .HasSubCommands}}{{.UsageString}}{{end}}`)
 
-listCmd.Flags().BoolVarP(&Flags.common.nameSort, "name-sort", "n", false, wski18n.T("sorts a list alphabetically by entity name; only applicable within the limit/skip returned entity block"))
+	listCmd.Flags().BoolVarP(&Flags.common.nameSort, "name-sort", "n", false, wski18n.T("sorts a list alphabetically by entity name; only applicable within the limit/skip returned entity block"))
 
-    WskCmd.AddCommand(
-        actionCmd,
-        activationCmd,
-        packageCmd,
-        ruleCmd,
-        triggerCmd,
-        sdkCmd,
-        propertyCmd,
-        namespaceCmd,
-        listCmd,
-        apiCmd,
-    )
+	WskCmd.AddCommand(
+		actionCmd,
+		activationCmd,
+		packageCmd,
+		ruleCmd,
+		triggerCmd,
+		sdkCmd,
+		propertyCmd,
+		namespaceCmd,
+		listCmd,
+		apiCmd,
+	)
 
-    WskCmd.PersistentFlags().BoolVarP(&Flags.Global.Verbose, "verbose", "v", false, wski18n.T("verbose output"))
-    WskCmd.PersistentFlags().BoolVarP(&Flags.Global.Debug, "debug", "d", false, wski18n.T("debug level output"))
-    WskCmd.PersistentFlags().StringVar(&Flags.Global.Cert, "cert", "", wski18n.T("client cert"))
-    WskCmd.PersistentFlags().StringVar(&Flags.Global.Key, "key", "", wski18n.T("client key"))
-    WskCmd.PersistentFlags().StringVarP(&Flags.Global.Auth, "auth", "u", "", wski18n.T("authorization `KEY`"))
-    WskCmd.PersistentFlags().StringVar(&Flags.Global.Apihost, "apihost", "", wski18n.T("whisk API `HOST`"))
-    WskCmd.PersistentFlags().StringVar(&Flags.Global.Apiversion, "apiversion", "", wski18n.T("whisk API `VERSION`"))
-    WskCmd.PersistentFlags().BoolVarP(&Flags.Global.Insecure, "insecure", "i", false, wski18n.T("bypass certificate checking"))
+	WskCmd.PersistentFlags().BoolVarP(&Flags.Global.Verbose, "verbose", "v", false, wski18n.T("verbose output"))
+	WskCmd.PersistentFlags().BoolVarP(&Flags.Global.Debug, "debug", "d", false, wski18n.T("debug level output"))
+	WskCmd.PersistentFlags().StringVar(&Flags.Global.Cert, "cert", "", wski18n.T("client cert"))
+	WskCmd.PersistentFlags().StringVar(&Flags.Global.Key, "key", "", wski18n.T("client key"))
+	WskCmd.PersistentFlags().StringVarP(&Flags.Global.Auth, "auth", "u", "", wski18n.T("authorization `KEY`"))
+	WskCmd.PersistentFlags().StringVar(&Flags.Global.Apihost, "apihost", "", wski18n.T("whisk API `HOST`"))
+	WskCmd.PersistentFlags().StringVar(&Flags.Global.Apiversion, "apiversion", "", wski18n.T("whisk API `VERSION`"))
+	WskCmd.PersistentFlags().BoolVarP(&Flags.Global.Insecure, "insecure", "i", false, wski18n.T("bypass certificate checking"))
 }
