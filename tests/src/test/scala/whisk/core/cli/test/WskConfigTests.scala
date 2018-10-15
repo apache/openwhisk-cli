9740d5dfe0084a978c812982187cbb4eab0b3690
Mark Deuser <mdeuser@us.ibm.com>
2018-10-15 19:30:18 -0400

    golint package path update (#374)
    * golint package path update
    
    * use controller.protocol instead of hardcoding protocol in test
    
    * remove controller http port now that the http api is no longer exposed

--- tests/src/test/scala/whisk/core/cli/test/WskConfigTests.scala
+++ tests/src/test/scala/whisk/core/cli/test/WskConfigTests.scala
@@ -265,11 +265,13 @@ class WskConfigTests extends TestHelpers with WskTestHelpers {
     }
   }
 
-  it should "show api build using http apihost" in {
+  it should "show api build using explicit protocol for apihost" in {
     val tmpwskprops = File.createTempFile("wskprops", ".tmp")
     try {
       val env = Map("WSK_CONFIG_FILE" -> tmpwskprops.getAbsolutePath())
-      val apihost = s"http://${WhiskProperties.getBaseControllerAddress()}"
+      val controllerProtocol = WhiskProperties.getProperty("controller.protocol")
+      val apihost =
+        s"${controllerProtocol}://${WhiskProperties.getBaseControllerHost()}"
       wsk.cli(Seq("property", "set", "--apihost", apihost), env = env)
       val rr = wsk.cli(Seq("property", "get", "--apibuild", "-i"), env = env)
       rr.stdout should not include regex("""whisk API build\s*Unknown""")
