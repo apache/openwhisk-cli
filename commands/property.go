23b579054221434f74db50d82304fd184e717042
Pierluigi Di Lorenzo <pierluigi.dilorenzo@gmail.com>
2021-01-07 16:23:45 +0100

    remove trailing slash on apihost #481 (#485)

--- commands/property.go
+++ commands/property.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	homedir "github.com/mitchellh/go-homedir"
@@ -471,7 +472,7 @@ func loadProperties() error {
 	}
 
 	if apiHost, hasProp := props["APIHOST"]; hasProp {
-		Properties.APIHost = apiHost
+		Properties.APIHost = strings.TrimRight(apiHost, "/")
 	}
 
 	if apiHost := os.Getenv("WHISK_APIHOST"); len(apiHost) > 0 {
